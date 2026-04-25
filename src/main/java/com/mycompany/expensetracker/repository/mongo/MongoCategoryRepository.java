d162dfb29026b3def6b0293f74199ea57a6945ab
Abdul Kader <babu313136@gmail.com>
2026-04-25 13:46:24 +0200

    feat: implement MongoCategoryRepository using MongoDB collection

---
+++ src/main/java/com/mycompany/expensetracker/repository/mongo/MongoCategoryRepository.java
@@ -0,0 +1,59 @@
+package com.mycompany.expensetracker.repository.mongo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mycompany.expensetracker.model.Category;
+import com.mycompany.expensetracker.repository.CategoryRepository;
+
+public class MongoCategoryRepository implements CategoryRepository {
+
+	private static final String COLLECTION = "categories";
+
+	private final MongoCollection<Document> collection;
+
+	public MongoCategoryRepository(MongoDatabase database) {
+		this.collection = database.getCollection(COLLECTION);
+	}
+
+	@Override
+	public void save(Category category) {
+		collection.insertOne(toDocument(category));
+	}
+
+	@Override
+	public List<Category> findAll() {
+		List<Category> result = new ArrayList<>();
+		for (Document doc : collection.find()) {
+			result.add(fromDocument(doc));
+		}
+		return result;
+	}
+
+	@Override
+	public Category findById(String id) {
+		Document doc = collection.find(new Document("id", id)).first();
+		if (doc == null) {
+			return null;
+		}
+		return fromDocument(doc);
+	}
+
+	@Override
+	public void delete(String id) {
+		collection.deleteOne(new Document("id", id));
+	}
+
+	private Document toDocument(Category category) {
+		return new Document("id", category.getId())
+				.append("name", category.getName());
+	}
+
+	private Category fromDocument(Document doc) {
+		return new Category(doc.getString("id"), doc.getString("name"));
+	}
+}
